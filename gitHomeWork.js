61508337f91199cbc95bae6eb31012e4b2045263
matanelVatkin <matanelVatkin@gmail.com>
2022-11-06 11:43:45 +0200

    adding log print

--- gitHomeWork.js
+++ gitHomeWork.js
@@ -1,3 +1,11 @@
 function main(){
     console.log("mergFF");
+    console.log("adding a commit to mergeing");
+    console.log("adding a commit to mergeing");
+    console.log("adding a commit to mergeing");
+    console.log("adding a commit to mergeing");
+    console.log("adding a commit to mergeing");
 }
+
+
+
